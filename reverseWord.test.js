5df1393986129c9a72f21480bf7ea4ec781b0834
Berke <berke.sutcu@momentup.com>
2024-03-07 15:54:31 +0300

    initial commit

---
+++ reverseWord.test.js
@@ -0,0 +1,13 @@
+const reverseWords = require("./reverseWords");
+
+test("should output tpircSavaJ si emosewa", () => {
+  expect(reverseWords("JavaScript is awesome")).toBe("tpircSavaJ si emosewa");
+});
+
+test("should output olleH, !dlroW", () => {
+  expect(reverseWords("Hello, World!")).toBe("olleH, !dlroW");
+});
+
+test("should output Lütfen bir cümle giriniz.", () => {
+  expect(reverseWords("")).toBe("Lütfen bir cümle giriniz.");
+});
---
+++ reverseWords.js
@@ -0,0 +1,5 @@
+function reverseWords(sentence) {
+  // Kodunuzu buraya yazınız.
+}
+
+module.exports = reverseWords;
