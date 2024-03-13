1233075b044ecb287e366df16204f5203a987e0e
berke-momentup <110673193+berke-momentup@users.noreply.github.com>
2024-03-13 15:30:36 +0300

    Update reverseWord.test.js

--- reverseWord.test.js
+++ reverseWord.test.js
@@ -5,7 +5,7 @@ test("should output tpircSavaJ si emosewa", () => {
 });
 
 test("should output olleH, !dlroW", () => {
-  expect(reverseWords("Hello, World!")).toBe("olleH, !dlroW");
+  expect(reverseWords("Hello, World!")).toBe(",olleH !dlroW");
 });
 
 test("should output Lütfen bir cümle giriniz.", () => {
